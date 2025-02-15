df6859d4f1768a55aac532f75a3e7afbf9f98268
Difficult_Bit2901 <dbhero2901@gmail.com>
2025-02-15 13:56:45 +0100

    Added searching for crates

--- src/api.rs
+++ src/api.rs
@@ -1,6 +1,47 @@
-const USER_AGENT: &'static str = "cargoman (https://github.com/DifficultBit2901/cargoman)";
-const RATE_LIMIT_MS: u32 = 1500;
+use std::time::Duration;
 
-pub fn search_pattern(pattern: &str) {
-    println!("Searching for '{}'...", pattern);
+use colored::Colorize;
+use crates_io_api::{Crate, CratesQuery, Sort, SyncClient};
+
+const USER_AGENT: &'static str = "cargoman (https://github.com/DifficultBit2901/cargoman)";
+const RATE_LIMIT_MS: u64 = 1500;
+
+fn get_client() -> SyncClient {
+    // Err is with invalid header, but that shouldn't change once built
+    SyncClient::new(USER_AGENT, Duration::from_millis(RATE_LIMIT_MS)).unwrap()
+}
+
+pub fn search_pattern(pattern: &str, limit: u8, colored: bool) {
+    let client = get_client();
+    let query = CratesQuery::builder()
+        .sort(Sort::Relevance)
+        .search(pattern)
+        .page_size(limit as u64)
+        .build();
+    let response = client.crates(query);
+    match response {
+        Err(e) => eprintln!("{}", e),
+        Ok(page) => {
+            for crt in page.crates {
+                print_crate_short(crt, colored);
+            }
+        }
+    }
+}
+
+fn print_crate_short(crt: Crate, colored: bool) {
+    let name = crt.name;
+    let keywords = crt.keywords;
+    if colored {
+        print!("{}", name.green());
+        if let Some(keywords) = keywords {
+            print!("({})", keywords.join(", ").blue());
+        }
+    } else {
+        print!("{}", name);
+        if let Some(keywords) = keywords {
+            print!("({})", keywords.join(", "));
+        }
+    }
+    println!();
 }
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ mod api;
 struct Cli {
     #[command(subcommand)]
     command: Commands,
+    #[arg(long, short, default_value = "false")]
+    colored: bool,
 }
 
 #[derive(Subcommand, Debug)]
@@ -15,12 +17,15 @@ enum Commands {
     Search {
         /// PATTERN to search for
         pattern: String,
+        #[arg(default_value = "10", short, long, value_parser=clap::value_parser!(u8).range(1..=30))]
+        limit: u8,
     },
 }
 
 fn main() {
     let cli = Cli::parse();
+    let colored = cli.colored;
     match &cli.command {
-        Commands::Search { pattern } => api::search_pattern(pattern),
+        Commands::Search { pattern, limit } => api::search_pattern(pattern, *limit, colored),
     }
 }
