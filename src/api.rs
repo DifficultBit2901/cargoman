bf4da35fbae58b387a908fdb772b2b0ffe41214c
Difficult_Bit2901 <dbhero2901@gmail.com>
2025-02-15 13:24:59 +0100

    Added Repository to user agent

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,4 @@
-const USER_AGENT: &'static str = "cargoman ()";
+const USER_AGENT: &'static str = "cargoman (https://github.com/DifficultBit2901/cargoman)";
 const RATE_LIMIT_MS: u32 = 1500;
 
 pub fn search_pattern(pattern: &str) {
